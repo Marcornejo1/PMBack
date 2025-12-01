919a84260f62e9e8c97e2fa04e8ecd4a854bd464
Marcornejo1 <marcodavid1211@gmail.com>
2025-12-01 10:32:27 -0600

    creamos archivos para uso con docker

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ import cors from "cors";
 //Configuraciones generales para la aplicación express
 const app= express();
 app.use(express.json());
+
+//Configuramos cors para el envío y uso de cookies al frontend
+const FRONTEND_HOST = process.env.FRONTEND_HOST || "http://localhost";
