f83a2b130e97a3df03517e89038c95f48b795bb3
Marcornejo1 <marcodavid1211@gmail.com>
2025-12-02 14:22:01 -0600

    Creado codigo para conexion a base de datos

---
+++ src/model/dbConnection.model.ts
@@ -0,0 +1,35 @@
+import sql, { ConnectionPool } from 'mssql';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const { DB_USER, DB_PASS, DB_NAME } = process.env;
+
+//Creamos configuraciones de sql
+const config: sql.config = {
+  server: DB_HOST,
+//Analizar donde colocar las credenciales
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+  options: {
+    encrypt: false,
+    trustedConnection: true,
+  }
+};
+
+let connectionPool: ConnectionPool | null = null;
+
+//Creamos la función de conexión a la base de datos
+const connectDB = async (): Promise<sql.ConnectionPool> => {
+  if (connectionPool)
+    return connectionPool;
+  try {
+    connectionPool = await sql.connect(config);
+    console.log("Conexión exitosa");
+    return connectionPool;
+  } catch (err) {
+    console.error("Error de conexión");
+    throw err;
+  }
+};
+
+export default connectDB;
---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+
+const authRouter: any = express.Router();
+
+export default authRouter;
