875c8c943b02304db7afb7630e0d0cc6f77e4b43
Marcornejo1 <marcodavid1211@gmail.com>
2025-12-04 14:32:01 -0600

    finalizado el index para el backend

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,22 @@ app.use(express.json());
 
 //Configuramos cors para el envío y uso de cookies al frontend
 const FRONTEND_HOST = process.env.FRONTEND_HOST || "http://localhost";
+const FRONTEND_PORT = process.env.FRONTEND_PORT || "5151";
+
+app.use(cors({
+  origin: [`${FRONTEND_HOST}:${FRONTEND_PORT}`],
+  credentials: true
+}));
+
+app.get('/api', (_, res) => {
+  res.send("Bienvenido a la API del sistema de generacion de reportes");
+});
+
+//Rutas de la api
+app.use("/api/auth", authRouter);
+
+
+const host = process.env.BACKEND_HOST || "http://localhost";
+const port = process.env.BACKEND_PORT || "3015";
+
+app.listen(port, () => console.log(`Escuchando en ${host}:${port}/`));
