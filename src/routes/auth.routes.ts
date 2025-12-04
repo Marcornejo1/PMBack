383e5303d9a9ec63ce79dd13c2531337f0a41d4a
Marcornejo1 <marcodavid1211@gmail.com>
2025-12-04 14:32:43 -0600

    Iniciamos el agregar rutas para su uso

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,9 @@
-import express from 'express';
+import express from "express";
+import authController from "../controllers/auth.controller";
 
 const authRouter: any = express.Router();
+
+authRouter.post("/", authController.login);
+authRouter.post("/refresh", authController.refresh);
 
 export default authRouter;
