887810128071d647ffc6b0b43bba3980f01b20c5
Marcornejo1 <marcodavid1211@gmail.com>
2025-12-22 17:00:45 -0600

    Arreglo a conexion

--- src/model/dbConnection.model.ts
+++ src/model/dbConnection.model.ts
@@ -1,12 +1,12 @@
 import sql, { ConnectionPool } from 'mssql';
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
-const { DB_USER, DB_PASS, DB_NAME } = process.env;
+const { DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env;
 
 //Creamos configuraciones de sql
 const config: sql.config = {
   server: DB_HOST,
-  port: parseInt(process.env.DB_PORT || '1466'),
+  port: parseInt(DB_PORT || '1466', 10),
 //Analizar donde colocar las credenciales
   user: DB_USER,
   password: DB_PASS,
